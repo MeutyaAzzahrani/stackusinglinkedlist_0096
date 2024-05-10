bd17177533af617c54cb5bd2148f5aa143cd9076
LENOVO <MeutyaAzzahrani>
2024-05-10 17:37:56 +0700

    membuat case 4

--- stackusinglinkedlist_0096/stackusinglinkedlist_0096.cpp
+++ stackusinglinkedlist_0096/stackusinglinkedlist_0096.cpp
@@ -96,7 +96,14 @@ int main() {
 				}
 				break;
 
+			case 4:
+				cout << "Exiting program." << endl;
+				break;
+			default:
+				cout << "Invalid choice. Try again." << endl;
+				break;
 		}
-
+		cout << endl;
 	}
+	return 0;
 }
