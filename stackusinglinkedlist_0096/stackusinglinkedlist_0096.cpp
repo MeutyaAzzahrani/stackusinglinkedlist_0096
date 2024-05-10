31d7fa8dd6f836536e0c0cb9ddfeab9a17836ac8
LENOVO <MeutyaAzzahrani>
2024-05-10 16:55:01 +0700

    membuat class stack

--- stackusinglinkedlist_0096/stackusinglinkedlist_0096.cpp
+++ stackusinglinkedlist_0096/stackusinglinkedlist_0096.cpp
@@ -10,3 +10,22 @@ public:
 		next = NULL;
 	}
 };
+
+class Stack {
+private:
+	Node* top;
+
+public:
+	Stack() {
+		top = NULL;
+	}
+
+	int push(int value) {
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "push value: " << value << endl;
+		return value;
+	}
+};
